2e7fbff54ef2c1b18c5e0acfa7ad20b09ffd0c69
Kleber Cabral <kleber.cabral@queensu.ca>
2023-08-10 14:18:13 -0400

    added ros2_control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -6,6 +6,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
@@ -19,10 +20,12 @@ def generate_launch_description():
 
     package_name='nav2_bot' 
 
+    use_ros2_control = LaunchConfiguration('use_ros2_control',default='true')
+
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true','use_ros2_control':use_ros2_control}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
@@ -38,10 +41,23 @@ def generate_launch_description():
                         output='screen')
 
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_controller"],
+    )
+
+    joint_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broadcaster"],
+    )
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_drive_spawner,
     ])
